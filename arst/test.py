e87597903da0eae54468029f3fc2627f9acf46d7
potato03kth <potatokth@gmail.com>
2023-12-13 02:35:15 +0900

    resetting

---
+++ arst/test.py
@@ -0,0 +1,15 @@
+N = input("")
+
+for i in range(N):
+    if (i == N):
+        # * 이 밑의 print 문은 i의 값과 n의 값이 같을 때, 출력됩니다
+
+        print("f{N}은 {I}와 같다 시발아")
+
+
+# TODO 이 함수는 유저로부터 정보를 받아야 합니다
+def GetElementFromUser ():
+    # ! 절대! 유저의 동의없이 리턴되선 안된다.``
+    return
+
+
